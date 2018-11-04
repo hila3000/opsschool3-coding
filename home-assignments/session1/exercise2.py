319d0ae90730d38a4c7b7f51d2c2189907b35f56
Hila Fish <hilavalensia@gmail.com>
2018-11-04 22:16:56 +0200

    added main function

--- home-assignments/session1/exercise2.py
+++ home-assignments/session1/exercise2.py
@@ -32,27 +32,35 @@ def weather_request(weatherapi_url):
     return json_data
 
 
-my_city, my_country = current_location(location_by_ip_api_url)
-
-custom_weather_api_url = weather_api_url.replace("{city}", my_city)
-
-weather_request_data = weather_request(custom_weather_api_url)
-weather_description = weather_request_data["weather"][0]["description"]
-weather_temperature = pytemperature.k2c(weather_request_data["main"]["temp"])
-
-country_code = weather_request_data["sys"]["country"]
-country_name = country_alpha2_to_country_name(country_code)
+def write_my_city_weather_to_file(my_city, my_country, weather_description, weather_temperature):
+    # Writes the weather info in my city to file
+    with open("{}weather_in_{}.txt".format("", my_city), "w") as text_file:
+        print("Want to know what's the weather in", my_city, "? Go to the text file created!")
+        print("The weather in {}, {}: {} with temperature of {} Celsius degrees".format(my_city, my_country,
+                                                                                        weather_description,
+                                                                                        int(weather_temperature)),
+                                                                                        file=text_file)
 
 
-with open("{}weather_in_{}.txt".format("", my_city), "w") as text_file:  # Writes the weather info in my city to file
-    print("Want to know what's the weather in", my_city, "? Go to the text file created!")
-    print("The weather in {}, {}: {} with temperature of {} Celsius degrees".format(my_city, my_country, weather_description,
-                                                                                    int(weather_temperature)), file=text_file)
+def check_weather_in_listed_cities(worldwide_cities):
+    for city in worldwide_cities:
+        custom_weather_api_url = weather_api_url.replace("{city}", city)
+        weather_request_data = weather_request(custom_weather_api_url)
+        weather_temperature = pytemperature.k2c(weather_request_data["main"]["temp"])
+        country_code = weather_request_data["sys"]["country"]
+        country_name = country_alpha2_to_country_name(country_code)
+        print("The weather in", city, ', ' + country_name, "is", int(weather_temperature), "Celsius degrees.")
 
-for city in worldwide_cities:
-    custom_weather_api_url = weather_api_url.replace("{city}", city)
+
+def main():
+    my_city, my_country = current_location(location_by_ip_api_url)
+    custom_weather_api_url = weather_api_url.replace("{city}", my_city)
     weather_request_data = weather_request(custom_weather_api_url)
+    weather_description = weather_request_data["weather"][0]["description"]
     weather_temperature = pytemperature.k2c(weather_request_data["main"]["temp"])
-    country_code = weather_request_data["sys"]["country"]
-    country_name = country_alpha2_to_country_name(country_code)
-    print("The weather in", city, ', ' + country_name, "is", int(weather_temperature), "Celsius degrees.")
+    write_my_city_weather_to_file(my_city, my_country, weather_description, weather_temperature)
+    check_weather_in_listed_cities(worldwide_cities)
+
+
+if __name__ == "__main__":
+    main()
